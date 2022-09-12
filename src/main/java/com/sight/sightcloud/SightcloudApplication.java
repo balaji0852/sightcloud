6b85dd1279ac02eea7f8a279fdac6b0477fd9c04
balaji0852 <balajikumar189@gmail.com>
2022-09-12 17:38:25 +0530

    cors fix, sig 31- new registry config

--- src/main/java/com/sight/sightcloud/SightcloudApplication.java
+++ src/main/java/com/sight/sightcloud/SightcloudApplication.java
@@ -2,9 +2,12 @@ package com.sight.sightcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -12,5 +15,16 @@ public class SightcloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SightcloudApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://edmsadmin.azurewebsites.net");
+			}
+		};
 	}
 }
