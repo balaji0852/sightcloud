033d875829ad479afe8ba98a4fc880eaf4beb0f3
balaji <balajikumar189@gmail.com>
2022-09-28 09:37:32 +0530

    l2 on

--- src/main/java/com/sight/sightcloud/SightcloudApplication.java
+++ src/main/java/com/sight/sightcloud/SightcloudApplication.java
@@ -1,19 +1,14 @@
 package com.sight.sightcloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class,  RabbitAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class,  RabbitAutoConfiguration.class})
 @RestController
 public class SightcloudApplication {
 
